e07d757a8d2d578db9393a691d63cf71875af1e8
nDr3K <andrea.cannavo.7@gmail.com>
2022-05-19 11:45:30 +0200

    aggiunti commenti

--- src/main/java/it/giornale/controller/AdminController.java
+++ src/main/java/it/giornale/controller/AdminController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -27,5 +29,13 @@ public class AdminController
 		
 		model.addAttribute("users", userService.readAll());
 		return "admin";
+	}
+	
+	//cancella utenti
+	@PostMapping
+	public String deleteUser(@ModelAttribute("user") User user)
+	{
+		userService.deleteUser(user);
+		return "redirect:/admin";
 	}
 }
