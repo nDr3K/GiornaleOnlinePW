06888be90c48236744e476d2ba36cd8ab9e29c5c
nDr3K <andrea.cannavo.7@gmail.com>
2022-05-20 09:32:36 +0200

    aggiunti delete/edit nel pannelloadmin

--- src/main/java/it/giornale/service/CategoryService.java
+++ src/main/java/it/giornale/service/CategoryService.java
@@ -7,6 +7,8 @@ import it.giornale.model.Category;
 public interface CategoryService 
 {
 	void create(Category c);
+	void update(Category c);
 	void remove(Category c);
 	List<Category> readAll();
+	Category readById(int id);
 }
